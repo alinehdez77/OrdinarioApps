f13f341363d1579c2a0286ae53b4cc5b4047129a
Aline Montserrat <alinemhdez@gmail.com>
2019-12-05 15:43:31 -0600

    Registro

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const Usuario = require("../dataaccess/model/Usuario");
+const jwt = require("jsonwebtoken");
+const config = require("../../config");
+const usuarioRouter = require("./api.usuario.route");
+router.use("/Usuario", usuarioRouter);
 
 
 router.post("/login", (req, res) => {
